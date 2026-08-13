170a62377a9ad27b8d9279f17b3858cf36e80311
bonnie <banusencan@gmail.com>
2026-08-13 17:51:31 +0100

    Day 8: XPath locators

--- quickChallengeday_8.py
+++ quickChallengeday_8.py
@@ -1,33 +1,29 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.get("https://www.saucedemo.com")
+driver.get("https://www.saucedemo.com")   # Step 1: we are on the LOGIN page now
 
+# Step 2: these three elements DO exist on the login page - safe to find here
 username_field = driver.find_element(By.XPATH, "//input[@id='user-name']")
 password_field = driver.find_element(By.XPATH, "//input[@id='password']")
 login_button = driver.find_element(By.XPATH, "//input[@id='login-button']")
-add_to_cart_button = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")   
 
-print("Username field:", username_field)
-print("Password field:", password_field)
-print("Login button:", login_button)
-print("Add to cart button:", add_to_cart_button)
-logo = driver.find_element(By.XPATH, "//div[@class='login_logo']")
-print("Logo:", logo)
+# Step 3: type credentials and submit - this is what moves us to the NEXT page
+username_field.send_keys("standard_user")
+password_field.send_keys("secret_sauce")
+login_button.click()
 
-username_field_by_attr = driver.find_element(By.XPATH, "//input[@data-test='username']")
-print("Username field (by data-test):", username_field_by_attr)
-username_field = driver.find_element(By.XPATH, "//input[@id='user-name']")
-password_field = driver.find_element(By.XPATH, "//input[@id='password']")
-login_button = driver.find_element(By.XPATH, "//input[@id='login-button']")
-login_credentials = driver.find_element(By.XPATH, "//div[@class='login_credentials_wrap']")
-print("Found login credentials box:", login_credentials)
+time.sleep(2)  # give the inventory page time to load
 
-import time
-time.sleep(1)  # give the page a moment to show the error
+# Step 4: ONLY NOW does this button exist, because we're on inventory.html
+add_to_cart_button = driver.find_element(By.XPATH, "//button[contains(@id, 'backpack')]")
+add_to_cart_button.click()
 
+# Step 5: this can happen anytime, since the logo exists on the LOGIN page, not inventory
+# (so actually this needs to happen back on the login page - see note below)
+
+time.sleep(3)
 driver.quit()
