ec6a5acdfd7746ed70fe9f4cbd1a223ef4d693d7
bonnie <banusencan@gmail.com>
2026-08-25 20:38:55 +0100

    Day 17: expand ProductsPage with cart and sort methods

--- pages/products_page.py
+++ pages/products_page.py
@@ -1,10 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 
 class ProductsPage:
     TITLE = (By.CSS_SELECTOR, ".title")
+    SORT_DROPDOWN = (By.CSS_SELECTOR, "select.product_sort_container")
+    FIRST_ITEM_PRICE = (By.CSS_SELECTOR, ".inventory_item_price")
+    CART_BADGE = (By.CSS_SELECTOR, ".shopping_cart_badge")
 
     def __init__(self, driver):
         self.driver = driver
@@ -17,3 +21,28 @@ class ProductsPage:
 
     def get_current_url(self):
         return self.driver.current_url
+
+    def add_item_to_cart(self, item_id):
+        locator = (By.ID, f"add-to-cart-sauce-labs-{item_id}")
+        button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(locator)
+        )
+        button.click()
+
+    def get_cart_badge_count(self):
+        badge = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.CART_BADGE)
+        )
+        return badge.text
+
+    def sort_by(self, value):
+        dropdown_element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.SORT_DROPDOWN)
+        )
+        Select(dropdown_element).select_by_value(value)
+
+    def get_first_item_price(self):
+        price_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.FIRST_ITEM_PRICE)
+        )
+        return price_element.text
--- test_login.py
+++ test_login.py
@@ -28,3 +28,27 @@ def test_multiple_invalid_logins(driver, username, password, expected_error):
 
     actual_error = login_page.get_error_message()
     assert expected_error in actual_error, f"Expected '{expected_error}' but got: '{actual_error}'"
+
+
+def test_add_item_to_cart_updates_badge(driver):
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.login("standard_user", "secret_sauce")
+
+    products_page = ProductsPage(driver)
+    products_page.add_item_to_cart("backpack")
+
+    badge_count = products_page.get_cart_badge_count()
+    assert badge_count == "1", f"Expected cart badge to show '1' but got '{badge_count}'"
+
+
+def test_sort_by_price_low_to_high(driver):
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.login("standard_user", "secret_sauce")
+
+    products_page = ProductsPage(driver)
+    products_page.sort_by("lohi")
+
+    first_price = products_page.get_first_item_price()
+    assert first_price == "$7.99", f"Expected '$7.99' but got '{first_price}'"
